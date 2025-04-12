65453e2332875034ee1e76a31e8c1e3af2d0880b
Edmund Wong <edmund.wong@live.ca>
2025-04-12 13:04:05 -0400

    add repos

---
+++ 3. ProvisionAppServer/modules/compute/variables.tf
@@ -0,0 +1,24 @@
+#-----compute/variables.tf-----
+#===============================
+variable "region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "ssh_key_public" {
+  type    = string
+  #Replace this with the location of you public key .pub
+  default = "~/.ssh/id_ed25519.pub"
+}
+
+variable "ssh_key_private" {
+  type    = string
+  #Replace this with the location of you private key
+  default = "~/.ssh/id_ed25519"
+}
+
+variable "subnet_ips" {}
+
+variable "security_group" {}
+
+variable "subnets" {}
