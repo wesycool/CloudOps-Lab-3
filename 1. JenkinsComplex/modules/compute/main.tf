6f81eca6daa0fdb3a4a8e269a6aeea1ff7f0b162
Edmund Wong <edmund.wong@live.ca>
2025-04-12 13:07:46 -0400

    removed Java node

--- 1. JenkinsComplex/modules/compute/main.tf
+++ 1. JenkinsComplex/modules/compute/main.tf
@@ -58,46 +58,6 @@ resource "aws_instance" "jenkins-master" {
  }
 }
 
-#Create and bootstrap Jenkins Node Server for Java
-#=================================================
-resource "aws_instance" "jenkins-node-java" {
-  instance_type               = "t2.micro"
-  ami                         = data.aws_ssm_parameter.webserver-ami.value
-  tags = {
-  Name = "jenkins_node_java"
-  }
-  key_name                    = aws_key_pair.aws-key.key_name
-  associate_public_ip_address = true
-  vpc_security_group_ids      = [var.security_group]
-  subnet_id                   = var.subnets
-
-  root_block_device {
-    volume_size = var.root_volume_size
-    volume_type = "gp3"
-  }
-  connection {
-      type        = "ssh"
-      user        = "ec2-user"
-      private_key   = file(var.ssh_key_private)
-      host        = self.public_ip
-   }
-
-  # Copy the file from local machine to EC2
-  provisioner "file" {
-    source      = "${path.root}/install_jenkins_node_java.yaml"
-    destination = "install_jenkins_node_java.yaml"
-  }
-
-  # Execute a script on a remote resource
-  provisioner "remote-exec" {
-    inline = [
-      "sudo yum update -y && sudo yum install ansible -y && sudo yum install java-17-amazon-corretto-headless -y",
-      "sleep 60s",
-      "ansible-playbook install_jenkins_node_java.yaml"
-    ]
- }
-}
-
 #Create and bootstrap Jenkins Node Server for Python
 #===================================================
 resource "aws_instance" "jenkins-node-python" {
