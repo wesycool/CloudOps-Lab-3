4b6465686dc0c51f86aad1f4b1cab98aa0f57d8e
Edmund Wong <edmund.wong@live.ca>
2025-04-12 13:08:56 -0400

    removed Java node

--- 1. JenkinsComplex/modules/compute/outputs.tf
+++ 1. JenkinsComplex/modules/compute/outputs.tf
@@ -8,18 +8,6 @@ output "master_ip" {
   value = aws_instance.jenkins-master.public_ip
 }
 
-output "java_node_id" {
-  value =  aws_instance.jenkins-node-java.id
-}
-
-output "java_node_ip" {
-  value = aws_instance.jenkins-node-java.public_ip
-}
-
-output "java_node_private_ip" {
-  value = aws_instance.jenkins-node-java.private_ip
-}
-
 output "python_node_id" {
   value =  aws_instance.jenkins-node-python.id
 }
